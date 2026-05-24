Kaloyan501/linux_msm89x7

pmdomain: qcom: rpmpd: Add SDM429W power domains
Add support for SDM429W power domains for devices with PM660.

NOTE: SDM429W have a sleep only power domain what is not supported
with the upstream rpmpd driver.

Signed-off-by: Barnabás Czémán <barnabas.czeman@mainlining.org>

drivers/pmdomain/qcom/rpmpd.c
@@ -409,6 +409,34 @@ static struct rpmpd mx_s2a_corner_ao = {
 	.key = KEY_CORNER,
 };
 
+static struct rpmpd mx_s2a_lvl_ao;
+static struct rpmpd mx_s2a_lvl = {
+	.pd = { .name = "mx", },
+	.peer = &mx_s2a_lvl_ao,
+	.res_type = RPMPD_SMPA,
+	.res_id = 2,
+	.key = KEY_LEVEL,
+};
+
+static struct rpmpd mx_s2a_lvl_ao = {
+	.pd = { .name = "mx_ao", },
+	.peer = &mx_s2a_lvl,
+	.active_only = true,
+	.res_type = RPMPD_SMPA,
+	.res_id = 2,
+	.key = KEY_LEVEL,
+};
+
+/* Sleep only is not supported yet
+static struct rpmpd mx_s2a_level_so = {
+	.pd = { .name = "mx_so", },
+	.peer = &mx_s2a_lvl,
+	.sleep_only = true,
+	.res_type = RPMPD_SMPA,
+	.res_id = 2,
+	.key = KEY_LEVEL,
+}; */
+
 static struct rpmpd mx_rwmx0_lvl_ao;
 static struct rpmpd mx_rwmx0_lvl = {
 	.pd = { .name = "mx", },
@@ -526,6 +554,24 @@ static struct rpmpd md_s1a_vfc = {
 	.res_type = RPMPD_SMPA,
 	.res_id = 1,
 	.key = KEY_FLOOR_CORNER,
+};
+
+static struct rpmpd md_s6a_lvl_ao;
+static struct rpmpd md_s6a_lvl = {
+	.pd = { .name = "md", },
+	.peer = &md_s6a_lvl_ao,
+	.res_type = RPMPD_SMPA,
+	.res_id = 6,
+	.key = KEY_LEVEL,
+};
+
+static struct rpmpd md_s6a_lvl_ao = {
+	.pd = { .name = "md", },
+	.peer = &md_s6a_lvl,
+	.active_only = true,
+	.res_type = RPMPD_SMPA,
+	.res_id = 6,
+	.key = KEY_LEVEL,
 };
 
 /* LPI_CX */
@@ -850,6 +896,23 @@ static const struct rpmpd_desc qm215_desc = {
 	.max_state = RPM_SMD_LEVEL_TURBO,
 };
 
+static struct rpmpd *sdm429w_rpmpds[] = {
+	[SDM429W_VDDMD] =	&md_s6a_lvl,
+	[SDM429W_VDDMD_AO] =	&md_s6a_lvl_ao,
+	[SDM429W_VDDCX] =	&cx_s1a_lvl,
+	[SDM429W_VDDCX_AO] =	&cx_s1a_lvl_ao,
+	[SDM429W_VDDCX_VFL] =	&cx_s1a_vfl,
+	[SDM429W_VDDMX] =	&mx_s2a_lvl,
+	[SDM429W_VDDMX_AO] =	&mx_s2a_lvl_ao,
+//	[SDM429W_VDDMX_SO] =	&mx_s2a_lvl_so, // Sleep Only is not supported
+};
+
+static const struct rpmpd_desc sdm429w_desc = {
+	.rpmpds = sdm429w_rpmpds,
+	.num_pds = ARRAY_SIZE(sdm429w_rpmpds),
+	.max_state = RPM_SMD_LEVEL_BINNING,
+};
+
 static struct rpmpd *sdm439_rpmpds[] = {
 	[SDM439_VDDMD] =	&md_s1a_lvl,
 	[SDM439_VDDMD_AO] =	&md_s1a_lvl_ao,
@@ -971,6 +1034,7 @@ static const struct of_device_id rpmpd_match_table[] = {
 	{ .compatible = "qcom,qcm2290-rpmpd", .data = &qcm2290_desc },
 	{ .compatible = "qcom,qcs404-rpmpd", .data = &qcs404_desc },
 	{ .compatible = "qcom,qm215-rpmpd", .data = &qm215_desc },
+	{ .compatible = "qcom,sdm429w-rpmpd", .data = &sdm429w_desc },
 	{ .compatible = "qcom,sdm439-rpmpd", .data = &sdm439_desc },
 	{ .compatible = "qcom,sdm660-rpmpd", .data = &sdm660_desc },
 	{ .compatible = "qcom,sm6115-rpmpd", .data = &sm6115_desc },

kadir1243/linux

drivers/pmdomain/qcom/rpmpd.c
@@ -409,6 +409,24 @@ static struct rpmpd mx_s2a_corner_ao = {
 	.key = KEY_CORNER,
 };
 
+static struct rpmpd mx_s2a_lvl_ao;
+static struct rpmpd mx_s2a_lvl = {
+	.pd = { .name = "mx", },
+	.peer = &mx_s2a_lvl_ao,
+	.res_type = RPMPD_SMPA,
+	.res_id = 2,
+	.key = KEY_LEVEL,
+};
+
+static struct rpmpd mx_s2a_lvl_ao = {
+	.pd = { .name = "mx_ao", },
+	.peer = &mx_s2a_lvl,
+	.active_only = true,
+	.res_type = RPMPD_SMPA,
+	.res_id = 2,
+	.key = KEY_LEVEL,
+};
+
 static struct rpmpd mx_rwmx0_lvl_ao;
 static struct rpmpd mx_rwmx0_lvl = {
 	.pd = { .name = "mx", },
@@ -515,6 +533,13 @@ static struct rpmpd md_s1a_vfc = {
 	.res_type = RPMPD_SMPA,
 	.res_id = 1,
 	.key = KEY_FLOOR_CORNER,
+};
+
+static struct rpmpd md_s6a_lvl = {
+	.pd = { .name = "md", },
+	.res_type = RPMPD_SMPA,
+	.res_id = 6,
+	.key = KEY_LEVEL,
 };
 
 /* LPI_CX */
@@ -831,6 +856,21 @@ static const struct rpmpd_desc qm215_desc = {
 	.max_state = RPM_SMD_LEVEL_TURBO,
 };
 
+static struct rpmpd *sdm429w_rpmpds[] = {
+	[SDM429W_VDDMD] =	&md_s6a_lvl,
+	[SDM429W_VDDCX] =	&cx_s1a_lvl,
+	[SDM429W_VDDCX_AO] =	&cx_s1a_lvl_ao,
+	[SDM429W_VDDCX_VFL] =	&cx_s1a_vfl,
+	[SDM429W_VDDMX] =	&mx_s2a_lvl,
+	[SDM429W_VDDMX_AO] =	&mx_s2a_lvl_ao,
+};
+
+static const struct rpmpd_desc sdm429w_desc = {
+	.rpmpds = sdm429w_rpmpds,
+	.num_pds = ARRAY_SIZE(sdm429w_rpmpds),
+	.max_state = RPM_SMD_LEVEL_TURBO,
+};
+
 static struct rpmpd *sdm439_rpmpds[] = {
 	[SDM439_VDDMD] =	&md_s1a_lvl,
 	[SDM439_VDDCX] =	&cx_s2a_lvl,
@@ -950,6 +990,7 @@ static const struct of_device_id rpmpd_match_table[] = {
 	{ .compatible = "qcom,qcm2290-rpmpd", .data = &qcm2290_desc },
 	{ .compatible = "qcom,qcs404-rpmpd", .data = &qcs404_desc },
 	{ .compatible = "qcom,qm215-rpmpd", .data = &qm215_desc },
+	{ .compatible = "qcom,sdm429w-rpmpd", .data = &sdm429w_desc },
 	{ .compatible = "qcom,sdm439-rpmpd", .data = &sdm439_desc },
 	{ .compatible = "qcom,sdm660-rpmpd", .data = &sdm660_desc },
 	{ .compatible = "qcom,sm6115-rpmpd", .data = &sm6115_desc },